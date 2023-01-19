2f709460474727006181f2663a616798c82e8206
Erick Serejo <erickrainier@hotmail.com>
2023-01-19 14:08:18 -0300

    import de tailwind

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import TaillWind from "./plugins/index.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App, TaillWind),
+}).$mount("#app");
