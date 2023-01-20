0ce67cbd6b1993c1ac3b7446b3eaa5d718df633e
Erick Serejo <erickrainier@hotmail.com>
2023-01-20 15:10:14 -0300

    importação de fonts

--- src/App.vue
+++ src/App.vue
@@ -88,12 +88,13 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto Condensed, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 </style>
